da50e022d7262ae84bc1c060e7a6e57ace35d44d
ethabhijit <coder.abhijit90@gmail.com>
2022-05-29 12:22:54 +0530

    update: contract name

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,12 +7,12 @@ async function main() {
   const factoryAddress = "0x02650a330F980362e7B233CF80b96D0Db04293b2";
   const WBRISEAddress = "0x0eb9036cbE0f052386f36170c6b07eF0a0E3f710";
 
-  const SwapRouter = await hre.ethers.getContractFactory("BakerySwapRouter");
+  const SwapRouter = await hre.ethers.getContractFactory("BitgertSwapRouter");
   const swapRouter = await SwapRouter.deploy(factoryAddress, WBRISEAddress);
 
   await swapRouter.deployed();
 
-  console.log("Router deployed to:", swapRouter.address);
+  console.log("Router deployed to: ", swapRouter.address);
 }
 
 main()
